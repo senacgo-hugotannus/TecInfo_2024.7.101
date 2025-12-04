65ad19ee2ffa5101fd5b594fb4d3654d2a59dbaf
lrnckaua <lrnckaua.pro@gmail.com>
2025-12-03 21:25:48 -0300

    pronto

--- UC_12/Entregas/Registros_e_Dicionarios/grupo_3/texto1/palmeiras não tem mundial.py
+++
@@ -1,7 +0,0 @@
-A = int(input("qual o ano do mundial do palmeiras  "))
-if A == 51 :
-    print ("51 é cachaça")
-elif A == 1951 :
-    print ("51 é cachaça")
-else:
-    print ("e por acaso o palmeiras tem mundial")
