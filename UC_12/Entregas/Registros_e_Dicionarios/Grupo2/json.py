b2febfd6acdd13a8e9af6c458eaa8b4f0584ddf0
Pedro Elias <pedroeliasbarbosa0@gmail.com>
2025-12-09 20:16:22 -0300

    Foi identado novamete

--- UC_12/Entregas/Registros_e_Dicionarios/Grupo2/json.py
+++ UC_12/Entregas/Registros_e_Dicionarios/Grupo2/json.py
@@ -1,43 +1,43 @@
 comp = {
-    "processador": {
-        "marca": "intel",
-        "série": "12400F",
-        "quantidade de núcleos": "8",
+    "Processador": {
+        "Marca": "intel",
+        "Série": "12400F",
+        "QuantidadeDeNúcleos": "8",
         "Tipo":"Core i5",
-        "cache": "L1",
-        "geração": "12th",
-        "Velocidade do processador": "3.40GZ",
-        "Tamanho da memória externa":"8 GB",
-        "Tecnologia da memória":"DDR",
+        "Cache": "L1",
+        "Geracao": "12th",
+        "VelocidadeDoProcessador": "3.40GZ",
+        "TamanhoDaMemóriaExterna":"8 GB",
+        "TecnologiaDaMemória":"DDR",
         "Voltagem":"bivolt",
-        "Potência em watts":"65 watts",
+        "PotênciaEmWatts":"65 watts",
     },
-    "placa_de_video":{
-        "marca": "MSI",
-        "série": "36NOL7MD1VOC",
+    "Placa_de_video":{
+        "Marca": "MSI",
+        "Série": "36NOL7MD1VOC",
         "Tipo":"GDDR6",
         "Velocidade": "1777 MHz",
         "VRAM":"12GB",
         "Voltagem":"bivolt",
         "Potência em watts":"170 watts",
     },
-    "fonte":{
-        "marca": "Corsair",
-        "série": "CP-9020278-BR",
+    "Fonte":{
+        "Marca": "Corsair",
+        "Série": "CP-9020278-BR",
         "Tipo":"ATX",
-        "Potência em watts":"650 watts",
-        "voltagem":"bivolt",
+        "PotênciaEmWatts":"650 watts",
+        "Voltagem":"bivolt",
     },
-    "armazenamento":{
-        "marca": "Kingston",
-        "série": "SNV3S/1000G",
+    "Armazenamento":{
+        "Marca": "Kingston",
+        "Série": "SNV3S/1000G",
         "Tipo":"DIMM",
         "Velocidade": "1777 MHz",
         "Voltagem":"28 V",
-        "Potência em watts":"50 watts",
+        "PotênciaEmWatts":"50 watts",
     },
 }
 
-print("\nEspecificações do processador:")
-for chave, valor in comp["processador"].items():
+print("\nEspecificações do Processador:")
+for chave, valor in comp["Processador"].items():
     print(f"- {chave}: {valor}")
