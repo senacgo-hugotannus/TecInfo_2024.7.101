d1312fa69d4ef9695392459924875963abb503ed
Juan carlos <jv365153@gmail.com>
2025-12-10 19:24:37 -0300

    correções de indentação

--- UC_12/Entregas/Registros_e_Dicionarios/PC.py
+++ UC_12/Entregas/Registros_e_Dicionarios/PC.py
@@ -1,192 +1,219 @@
 {
-        "processador":{
-        "fabricante": "intel",
-        "modelo": "intel i5",
-        "Geração": "12400f",
-        "socket" : "LGA 1700",
-        "frequencia Base": "2.5 Ghz",
-        "cache l1" : " 80 kb",
-        "cache l2" : "1,25mb",
-        "cache l3" : "18 mb",
-        "núcleo": "6",
-        "threads": "12",
-        "tdp" : "65w",
-        "suporte de memoria" : "128gb",
-        "tipo de memoria" : "DDR4""DDR5",
-        "virtualização" : "SIM",
-      },
-        "placa de vídeo":{
-        "fabricante" : "Nvidia",
-        "modelo"     : "RTX 2060",
-        "marca"      : "Geforce",
-        "Arquitetura": "NVIDIA Turing (TU106)",
-        "Largura de banda da memória": "336 GB/s",
-        "vram"       : "6 Gb",
-        "tipo de memoria" : "GDDR6",
-        "DirectX" : "12 ultimate (12_2)",
-        "PCI_ EX" : "3.0 x16",
-        "vulkan"    : "1.4",
-        "cache_L1"  : "64 kb (por SM)",
-        "cache_L2"  : "3 MB",
-        "Relógio de base" :"1365 MHz",
-        "Relógio de aumento" : "1680 MHz",
-        "ALIMENTAÇÃO " : " 4+2 6 PINOS"
-       },
-        "Placa Mãe" :{
-            "fabricante" : "Gigabyte",
-            "modelo"     : "H610M H V3 DDR4",
-            "formato"    : "M-ATX",
-            "Socket"     : "LGA 1700",
-            "Capacidade_Máxima de memória" : "64Gb 2 slots de memória DDR4 3200mhz",
-            "PCI_EX"    : ["4.0 x16","3.0 x1"],
-            "Suport_Cpu": ["intel Core 12° a 14° geração"],
-            "TDP": "125W",
-            "Armazenamento": "[1x conector M.2","2x conector SATA 6 GB/s]"
-            "USB " : "[4 x Portas USB 3.2 Gen 1","6 x Portas USB 2.0/1.1]"
-            "BIOS" : "UEFI",
-            "ALIMENTAÇÃO " : "1x Conector de alimentação principal ATX de 24 pinos",
-            "LAN" : "Realtek® GbE (1 Gbps/100 Mbps)",
-            "AUDIO" : "CODEC de áudio Realtek®  2/4/5.1/7.1 canais",
-            "Gráficos" : "Suporte para processador gráfico integrado", 
-            "resolução máxima" : "4096x2160 60 Hz",
-            "HDMI" : "Suporte para versão HDMI 2.0 e HDCP 2.3." ,
-        },
-        "Memoria ram" :{
-            "fabricante": "kingston",
-            "modelo"    : "Kingston Fury Beast DDR4",
-	        "capacidade": "8gb Ram",
-            "Tipo"      : "DDR4 SDRAM",
-            "Frequência": "3200MHz",
-            "Latência " : "CL16-18-18",
-            "Voltagem"  : "1,2V (padrão), 1,35V(XMP)",
-            "Compatibilidade" : "intel XMP 2.0, AMD Ryzen",
-            "Dissipador"  : "Alumínio preto, perfil",
-            "Organização" : "1Rx8 (single Rank)",
- 
+    "processador": {
+        "fabricante":"intel",
+        "modelo":"intel i5",
+        "geracao":"12400f",
+        "socket":"LGA 1700",
+        "frequencia_base":"2.5 Ghz",
+        "cache l1":"80 kb",
+        "cache l2":"1,25mb",
+        "cache l3":"18 mb",
+        "nucleo":"6",
+        "threads":"12",
+        "tdp":"65w",
+        "suporte_de_memoria":"128gb",
+        "tipo_de_memoria": [
+            "DDR4",
+            "DDR5"
+        ],
+        "virtualizacao": "SIM"
     },
-        "SSD NVME" :{
-            "Fabricante" : "Kingston",
-            "Modelo"     : "Kingston NV2 SSD",
-            "Capacidade" : "1TB",
-            "Interface"  : "PCIe 4.0 NVMe Gen 4x4",
-            "Formato"    : "M.2 2280",
-            "Leitura"    : "até 3500 MB/s",
-            "Gravação"   : "até 2100 MB/s",
-            "Consumo"    : "1,4W inativo/4,6W máximo",
-            "Temperatura": "0°C a 70°C(uso), -40°C a 85°C(armazenamento)",
-            "Resistência TBW" : "~320TB",
-            "MTBF"            : " 1,5 milhão de horas ",
-    }, 
-        "FONTE DE ALIMENTAÇÃO" :{
-            
-            "Fabricante" : "MSI",
-            "Formato" : "ATX",
-            "Potência" : "600W", 
-            "Certificação" : "80 Plus White",
-            "PFC": "ATIVO",
-            "Voltagem de entrada" : " 110–240V AUTO ",
-            "Frequência de entrada" : " 50–60 Hz ",
-            "Conectores": {
-                "Conector ATX 24 pinos" : "1x",
-                "EPS 8 pinos" : "1X",
-                "PCI-E 6+2 pinos" : "2x",
-                "SATA 15 pinos" : "5x",
-                "Molex 4 pinos" : "2x",
-                "FDD 4 pinos" : "1x",
-            },
-            "Resfriamento" : "Ventoinha: 120 mm",
-            "Rolamento" : "Hidráulico",
-            "Nível de ruído" : " 17,2 – 33,6 dBA",
-            " Dimensões: " : "150 x 140 x 86 mm",
-            "cor" : "preto"
-    },    
-        "Teclado" :{
-             "fabricante" : "SuperFrame"
-             "Modelo:" "SuperFrame Ribbon - Black/Grey",
-             "Tipo de teclado" : "Mecânico",
-             "formato" : "75% (compacto, sem teclado numérico , mas mantém teclas     essências)",
-             "Layout" : "ABNT2(padrão brasileiro ,com cedilha e acentos)",
-             "Switches": ["Red" "Hot-Swap"],
-             "conexão" : ["Com fio", "Bluetooth 2.4Ghz"],
-             "iluminação" : "RGB",
-             "Compatibilidade": ["Windows,macOS"],
-    
-    }, 
-        "Mouse" :{
-            "Marca" : "Attack Shark Modelo: X11 Branco",
-            "Sem Fio" : "Sim",
-            "Sensor" : "Pixart PAW3311",
-            "Resolução" : "até 22.000 DPI (6 níveis ajustáveis)",
-            "Switches": ["Huano/TTC"],
-            "Taxa de Polling" : "até 1.000 Hz",
-            "Velocidade de Rastreamento" : "até 400 IPS",
-            "Aceleração" : "até 40G",
-            "Peso" : "Aproximadamente 62g (Ultra Leve)",
-            "Design" : "Ergonômico",
-            "Iluminação" : "RGB com efeitos customizáveis",
-            "Software: Personalizável": "(ajuste de DPI, Polling Rate, Macros e RGB)",
-            "Modos_Conexão" :["USB-C", "Wireless 2.4GHz" , "Bluetooth 5.2"],
-            "Bateria" : "65 horas",
-            "Compatibilidade" : ["Windows, macOS, Linux"],
-            "Dock de Carregamento" : "sim",
-            "Cabo USB-C" : "sim",
-            "Receptor USB" : "Wireless 2.4GHz",
-            "Manual do Usuário" : "sim",
+    "placa_de_video": {
+        "fabricante":"Nvidia",
+        "modelo":"RTX 2060",
+        "marca":"Geforce",
+        "arquitetura":"NVIDIA Turing (TU106)",
+        "largura_de_banda_da_memoria":"336 GB/s",
+        "vram": "6 Gb",
+        "tipo_de_memoria": "GDDR6",
+        "directx":"12 ultimate (12_2)",
+        "pci_ex":"3.0 x16",
+        "vulkan":"1.4",
+        "cache_l1":"64 kb (por SM)",
+        "cache_l2":"3 MB",
+        "relogio_de_base":"1365 MHz",
+        "relogio_de_aumento":"1680 MHz",
+        "alimentacao ":"4+2 6 PINOS"
+    },
+    "placa_mae": {
+        "fabricante":"Gigabyte",
+        "modelo":"H610M H V3 DDR4",
+        "formato":"M-ATX",
+        "socket":"LGA 1700",
+        "capacidade_maxima_de_memoria":"64Gb 2 slots de memória DDR4 3200mhz",
+        "pci_ex": [
+            "4.0 x16",
+            "3.0 x1"
+        ],
+        "suport_cpu": [
+            "intel Core 12° a 14° geração"
+        ],
+        "tdp": "125W",
+        "armazenamento": [
+            "1x conector M.2",
+            "2x conector SATA 6 GB/s"
+        ],
+        "usb": [
+            "4 x Portas USB 3.2 Gen 1",
+            "6 x Portas USB 2.0/1.1"
+        ],
+        "bios": "UEFI",
+        "alimentacao ":"1x Conector de alimentação principal ATX de 24 pinos",
+        "lan":"Realtek® GbE (1 Gbps/100 Mbps)",
+        "audio":"CODEC de áudio Realtek®  2/4/5.1/7.1 canais",
+        "graficos":"Suporte para processador gráfico integrado",
+        "resolucao_maxima":"4096x2160 60 Hz",
+        "hdmi":"Suporte para versão HDMI 2.0 e HDCP 2.3."
+    },
+    "Memoria ram": {
+        "fabricante":"kingston",
+        "modelo":"Kingston Fury Beast DDR4",
+        "capacidade":"8gb Ram",
+        "tipo":"DDR4 SDRAM",
+        "frequencia":"3200MHz",
+        "latencia ":"CL16-18-18",
+        "voltagem":"1,2V (padrão), 1,35V(XMP)",
+        "compatibilidade":"intel XMP 2.0, AMD Ryzen",
+        "dissipador":"Alumínio preto, perfil",
+        "organizacao":"1Rx8 (single Rank)"
+    },
+    "sdd_nvme": {
+        "fabricante":"Kingston",
+        "modelo":"Kingston NV2 SSD",
+        "capacidade":"1TB",
+        "interface":"PCIe 4.0 NVMe Gen 4x4",
+        "formato":"M.2 2280",
+        "leitura":"até 3500 MB/s",
+        "gravacao":"até 2100 MB/s",
+        "consumo":"1,4W inativo/4,6W máximo",
+        "temperatura":"0°C a 70°C(uso), -40°C a 85°C(armazenamento)",
+        "resistencia_tbw":"~320TB",
+        "mtbf":"1,5 milhão de horas "
+    },
+    "fonte_de_alimentacao": {
+        "fabricante":"MSI",
+        "formato":"ATX",
+        "potencia":"600W",
+        "Certificacao":"80 Plus White",
+        "pfc": "ATIVO",
+        "voltagem_de_entrada":"110–240V AUTO ",
+        "frequencia_de_entrada":"50–60 Hz ",
+        "conectores": {
+            "conector_atx_24_pinos": "1x",
+            "eps 8 pinos": "1X",
+            "pci-E_6+2_pinos": "2x",
+            "sata_15_pinos": "5x",
+            "molex_4_pinos": "2x",
+            "fdd_4_pinos": "1x"
         },
-            
-        "Gabinete" :{
-            "fabricante" : "Gamer ninja",
-            "Modelo" : "Gamer Ninja Leaf",
-            "Formato" : "Mid Tower",
-            "Compatibilidade de Placas-mãe " : ["ATX, Micro-ATX, Mini-ITX"],
-            "Material" : ["Aço e vidro temperado"],
-            "Cor" : "Preto",
-            "Espaço" : "Interno",
-            "Placa de Vídeo" : "até 320 mm de comprimento",
-            "Cooler do processador" : "até 160 mm de altura",
-            "Fonte" : "padrão ATX",
-            "armazenamento" : ["2x HDD 3.5" , "2x SSD 2.5"],
-            "USB" : ["1x Usb 3.0", "2x Usb 2.0"],
-            "Audio" : ["entrada para microfone", "Saida fone"],
-            "controle RGB" : " sim",
-            "Dimensões" : "Altura 450 mm",
-            "largura" : "190 mm",
-            "profundidade" : "370 mm",
-            "Filtro de poeira" : "Removível ",  #na parte inferior",
-            "Estrutura otimizada" : "sim",
-        },
-            "Monitor" :{
-            "fabricante": "AOC",
-            "Modelo": "24G2E1",
-            "Tamanho": "23,8 polegadas",
-            "Painel": "IPS (16:9)",
-            "Resolução": "1920 x 1080 (Full HD)",
-            "Taxa de atualização": "100 Hz",
-            "Tempo de resposta": "1 ms",
-            "Pixel pitch": "0,2745 mm",
-            "Densidade": "92,5 PPI",
-            "Brilho": "250 cd/m²",
-            "Ângulos de visão": "178° (horizontal e vertical)",
-            "Cores": ["8 bits","sRGB ampla (107,4%)"],
-            "Plug & Play": "sim",
-            "Função OSD" : "sim",
-            "Cor do produto": "preto e vermelho",
-            "Compatibilidade": ["Windows, MacOS e Linux"],
-             
-        },            
-},
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        "resfriamento":"Ventoinha:120 mm",
+        "rolamento":"Hidráulico",
+        "nivel_de_ruido":"17,2 – 33,6 dBA",
+        " dimensoes":"150 x 140 x 86 mm",
+        "cor": "preto"
+    },
+    "teclado": {
+        "fabricante":"SuperFrame",
+        "modelo":"Ribbon Black/Grey",
+        "tipo_de_teclado":"Mecânico",
+        "formato":"75% (compacto, sem teclado numérico , mas mantém teclas     essências)",
+        "layout":"ABNT2(padrão brasileiro ,com cedilha e acentos)",
+        "switches": [
+            "Red",
+            "Hot-Swap"
+        ],
+        "conexao": [
+            "Com fio",
+            "Bluetooth 2.4Ghz"
+        ],
+        "iluminacao": "RGB",
+        "compatibilidade": [
+            "Windows,macOS"
+        ]
+    },
+    "mouse": {
+        "marca":"Attack Shark Modelo: X11 Branco",
+        "sem_fio":"Sim",
+        "sensor":"Pixart PAW3311",
+        "resolucao":"até 22.000 DPI (6 níveis ajustáveis)",
+        "switches": [
+            "Huano/TTC"
+        ],
+        "taxa_de_polling":"até 1.000 Hz",
+        "velocidade_de_rastreamento":"até 400 IPS",
+        "aceleração":"até 40G",
+        "peso":"Aproximadamente 62g (Ultra Leve)",
+        "design":"Ergonômico",
+        "iluminacao":"RGB com efeitos customizáveis",
+        "software_personalizavel":"(ajuste de DPI, Polling Rate, Macros e RGB)",
+        "modos_Conexao": [
+            "USB-C",
+            "Wireless 2.4GHz",
+            "Bluetooth 5.2"
+        ],
+        "bateria":"65 horas",
+        "Compatibilidade": [
+            "Windows, macOS, Linux"
+        ],
+        "dock_de_carregamento":"sim",
+        "cabo_usb-c":"sim",
+        "receptor_usb":"Wireless 2.4GHz",
+        "manual_do_usuario":"sim"
+    },
+    "gabinete": {
+        "fabricante":"Gamer ninja",
+        "modelo":"Gamer Ninja Leaf",
+        "formato":"Mid Tower",
+        "compatibilidade_de_placas-mae": [
+            "ATX, Micro-ATX, Mini-ITX"
+        ],
+        "material": [
+            "Aço e vidro temperado"
+        ],
+        "cor":"Preto",
+        "espaço":"Interno",
+        "placa_de_video":"até 320 mm de comprimento",
+        "cooler_do_processador":"até 160 mm de altura",
+        "fonte":"padrão ATX",
+        "armazenamento": [
+            "2x HDD 3.5",
+            "2x SSD 2.5"
+        ],
+        "usb": [
+            "1x Usb 3.0",
+            "2x Usb 2.0"
+        ],
+        "audio": [
+            "entrada para microfone",
+            "Saida fone"
+        ],
+        "controle_rgb":"sim",
+        "dimensoes":"Altura 450 mm",
+        "largura":"190 mm",
+        "profundidade":"370 mm",
+        "filtro_de_poeira":"Removível "
+    },
+    "monitor": {
+        "fabricante":"AOC",
+        "modelo":"24G2E1",
+        "tamanho":"23,8 polegadas",
+        "painel": "IPS (16:9)",
+        "resolucao":"1920 x 1080 (Full HD)",
+        "taxa_de_atualizacao":"100 Hz",
+        "tempo_de_resposta":"1 ms",
+        "pixel_pitch":"0,2745 mm",
+        "densidade":"92,5 PPI",
+        "brilho":"250 cd/m²",
+        "angulos_de_visão":"178° (horizontal e vertical)",
+        "cores": [
+            "8 bits",
+            "sRGB ampla (107,4%)"
+        ],
+        "plug_and_play":"sim",
+        "funcao_osd":"sim",
+        "cor_do_produto":"preto e vermelho",
+        "compatibilidade": [
+            "Windows, MacOS e Linux"
+        ]
+    }
+}
